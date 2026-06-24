49f2aae49ae15084ff89e5c7096f8365bc3c4c25
Luis Eduardo Faria Filho <luiseduardofariafilho@MacBook-Air-de-Luis.local>
2026-06-23 21:56:34 -0300

    feat: adiciona Meta Pixel (ID 1700853197905257) na LP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Sora, Inter, Cormorant_Garamond } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const sora = Sora({
@@ -62,7 +63,38 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${sora.variable} ${inter.variable} ${cormorant.variable} overflow-x-hidden`}
     >
-      <body className="overflow-x-hidden bg-offwhite text-grafite antialiased">{children}</body>
+      <head>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1700853197905257&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
+      <body className="overflow-x-hidden bg-offwhite text-grafite antialiased">
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1700853197905257');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
